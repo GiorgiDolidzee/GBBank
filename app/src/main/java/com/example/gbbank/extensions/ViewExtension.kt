a993dbc161b5d8ba2a3b152369ae326387fb2764
Bachana Mosulishvili <bachanamosulishvili@gmail.com>
2021-12-28 17:03:41 +0400

    1.1.6 Added Dynamic Currency Spinners for Currency Converter Improved UI and UX

--- app/src/main/java/com/example/gbbank/extensions/ViewExtension.kt
+++ app/src/main/java/com/example/gbbank/extensions/ViewExtension.kt
@@ -6,3 +6,15 @@ import com.google.android.material.snackbar.Snackbar
 fun View.showSnackBar(message: String, length: Int = Snackbar.LENGTH_LONG) {
     Snackbar.make(this, message, length).show()
 }
+
+fun View.show(){
+    this.visibility = View.VISIBLE
+}
+
+fun View.remove(){
+    this.visibility = View.GONE
+}
+
+fun View.hide(){
+    this.visibility = View.INVISIBLE
+}
--- app/src/main/java/com/example/gbbank/ui/exchange/ExchangeFragment.kt
+++ app/src/main/java/com/example/gbbank/ui/exchange/ExchangeFragment.kt
@@ -1,14 +1,21 @@
 package com.example.gbbank.ui.exchange
 
-import android.graphics.Color
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gbbank.R
 import com.example.gbbank.adapters.RatesAdapter
 import com.example.gbbank.databinding.FragmentExchangeBinding
+import com.example.gbbank.extensions.hide
+import com.example.gbbank.extensions.remove
+import com.example.gbbank.extensions.show
 import com.example.gbbank.extensions.showSnackBar
 import com.example.gbbank.model.ExchangeResponse
 import com.example.gbbank.model.Rates
@@ -27,26 +34,13 @@ class ExchangeFragment : BaseFragment<FragmentExchangeBinding>(FragmentExchangeB
     private lateinit var adapter: RatesAdapter
 
     override fun start() {
-        init()
-        listeners()
         getRates()
     }
 
-    private fun init(){
-        binding.spFrom.setSelection(1)
-        binding.spTo.setSelection(0)
-    }
 
     private fun listeners() {
         binding.btnConvert.setOnClickListener {
-            try {
-                val amount = binding.etAmount.text.toString().toInt()
-                val from = binding.spFrom.selectedItem.toString()
-                val to = binding.spTo.selectedItem.toString()
-                exchange(amount, from, to)
-            } catch (e: Exception) {
-                view?.showSnackBar(getString(R.string.sb_type_valid_number))
-            }
+
         }
         binding.ivSwap.setOnClickListener {
             val from = binding.spFrom
@@ -55,6 +49,27 @@ class ExchangeFragment : BaseFragment<FragmentExchangeBinding>(FragmentExchangeB
             val fromPosition = from.selectedItemPosition
             from.setSelection(to.selectedItemPosition)
             to.setSelection(fromPosition)
+        }
+        binding.etAmount.addTextChangedListener {
+            if (it!!.isEmpty()) {
+                binding.llResult.remove()
+            } else {
+                exchangeCheck()
+            }
+        }
+        binding.btnConvert.setOnClickListener {
+            exchangeCheck()
+        }
+    }
+
+    private fun exchangeCheck() {
+        try {
+            val amount = binding.etAmount.text.toString().toInt()
+            val from = binding.spFrom.selectedItem.toString()
+            val to = binding.spTo.selectedItem.toString()
+            exchange(amount, from, to)
+        } catch (e: Exception) {
+            view?.showSnackBar(getString(R.string.sb_type_valid_number))
         }
     }
 
@@ -67,6 +82,7 @@ class ExchangeFragment : BaseFragment<FragmentExchangeBinding>(FragmentExchangeB
                     is Resource.Success -> {
                         binding.progressBar.isVisible = false
                         rateOperations(it.data)
+                        makeSpinnerArray(it.data)
                     }
                     is Resource.Error -> {
                         binding.progressBar.isVisible = false
@@ -115,8 +131,32 @@ class ExchangeFragment : BaseFragment<FragmentExchangeBinding>(FragmentExchangeB
 
     private fun showExchangeResult(data: ExchangeResponse? = null) {
         if (data != null) {
-            val result = "${data.amount} ${data.from} = ${data.value} ${data.to}"
-            binding.tvResult.text = result
+            val from = "${data.amount} ${data.from}"
+            val to = "${data.value} ${data.to}"
+
+            binding.tvFrom.text = from
+            binding.tvTo.text = to
+
+            binding.llResult.show()
         }
+    }
+
+    private fun makeSpinnerArray(data: Rates?) {
+        val currencyList = mutableListOf<String>()
+        currencyList.add("GEL")
+        data?.commercialRatesList?.forEach {
+            currencyList.add(it?.currency.toString())
+        }
+        val currencyArray = currencyList.toTypedArray()
+        val arrayAdapter =
+            ArrayAdapter(requireContext(), R.layout.layout_spinner_item, currencyArray)
+
+        binding.spFrom.adapter = arrayAdapter
+        binding.spTo.adapter = arrayAdapter
+
+        binding.spFrom.setSelection(currencyArray.indexOf("USD"))
+        binding.spTo.setSelection(currencyArray.indexOf("GEL"))
+
+        listeners()
     }
 }
